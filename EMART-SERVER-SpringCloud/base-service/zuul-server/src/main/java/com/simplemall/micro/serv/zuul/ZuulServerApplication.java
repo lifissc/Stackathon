a09de3d2a9375ef2b921c637cdf4ab03af99d47f
lifissc <lifissc@cn.ibm.com>
2020-05-22 17:54:00 +0800

    zuul

---
+++ EMART-SERVER-SpringCloud/base-service/zuul-server/src/main/java/com/simplemall/micro/serv/zuul/ZuulServerApplication.java
@@ -0,0 +1,22 @@
+package com.simplemall.micro.serv.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * API Gateway
+ * 
+ * @author LiFeng
+ *
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
